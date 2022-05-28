322f36e2f78e3d3b72aec2cd3a148a5dda4e8127
Martinez0722 <102329554+Martinez0722@users.noreply.github.com>
2022-05-28 03:22:26 -0300

    .

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
-console.log("aoba")
+
+
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:5,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:3
+        }
+    }
+})
